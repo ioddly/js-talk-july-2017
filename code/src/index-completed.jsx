13d1f5da8ac6ba8173f69d533f1274419af4be54
Phil <phil@upvalue.io>
2017-07-18 23:58:46 -0500

    Update code, slides

---
+++ code/src/index-completed.jsx
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const Report = ({name, temp, humidity, description, zip}) => {
+  // const name = props.
+  return <div className="card mb-1">
+    <div className="card-block">
+      <p>{name} ({zip})</p>
+      <p>Temperature: {temp} *F</p>
+      <p>Humidity: {humidity}%</p>
+      <p>{description}</p>
+    </div>
+  </div>;
+}
+
+class WeatherApp extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      reports: []
+    };
+  }
+
+  async fetchWeather(zip) {
+    const response = await window.fetch(`http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=0fbbf897fdc8751bb1b5eb559319fe18&units=imperial`);
+    const json = await response.json();
+    
+    if(json) {
+      if (json.cod === '404') {
+        this.setState({ error: `${zip} is an invalid zip` });
+        return null;
+      }
+
+      const { name, main: { temp, humidity }, weather: [{description}] } = json;
+
+      const object = { name, temp, humidity, description, zip };
+      console.log(object);
+
+      const updatedReports = this.state.reports;
+      updatedReports.push(object);
+      this.setState({ reports: updatedReports, error: '' })
+    }
+  }
+
+  addZip(event) {
+    event.preventDefault();
+
+    const zipText = this.element.value;
+
+    if (zipText.length !== 5) {
+      this.setState({ error: `${zipText} should be 5 digits long`});
+      return;
+    }
+
+    const zipCode = parseInt(zipText, 10);
+
+    if (isNaN(zipCode)) {
+      this.setState({ error: `${zipText} is not a valid number`});
+      return;
+    }
+
+    this.fetchWeather(zipCode);
+  }
+
+  render() {
+    return <div className="container">
+      <div>
+        {this.state.error && <p>{this.state.error}</p>}
+
+        <form className="form-inline" onSubmit={event => this.addZip(event)}>
+          <input className="form-control" type="text" placeholder="Enter zip code"
+            ref={elt => this.element = elt} />
+          <button type="submit" className="btn btn-primary">Add zipcode</button>
+        </form>
+      </div>
+      <p>Displaying {this.state.reports.length} weather reports</p>
+      <button className="btn btn-secondary" onClick={() => this.setState({reports: []})}>Clear reports</button>
+      <div className="mt-1">
+        {this.state.reports.map((report, i) => <Report {...report} key={i} />)}
+      </div>
+    </div>;
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.render(<WeatherApp />, document.getElementById('root'));
+});
